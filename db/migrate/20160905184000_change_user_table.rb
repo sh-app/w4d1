f8b5ba733789d9335a36fef614e65fadefdfac38
peter & shahriver <peterzhen0713@gmail.com>
2016-09-05 14:47:47 -0400

    modify scheme for user

---
+++ db/migrate/20160905184000_change_user_table.rb
@@ -0,0 +1,6 @@
+class ChangeUserTable < ActiveRecord::Migration
+  def change
+    remove_column :users, :email, :string
+    rename_column :users, :name, :username
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905162729) do
+ActiveRecord::Schema.define(version: 20160905184000) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "email",      null: false
+    t.string   "username",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
